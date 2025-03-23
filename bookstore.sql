975fc97454d2ff299f25248d63f3c9af53a1f962
Sanchita Devi <thesanchitadevi@gmail.com>
2025-03-23 21:31:26 +0600

    Problem - 7. Increase the price of all books published before 2000 by 10%

--- bookstore.sql
+++ bookstore.sql
@@ -81,3 +81,8 @@ HAVING COUNT(orders.id) > 1;
 
 -- 6. Find the average price of books in the store 
 SELECT ROUND(AVG(price), 2) AS avg_book_price FROM books;
+
+-- 7. Increase the price of all books published before 2000 by 10%
+UPDATE books 
+SET price = price * 1.10 
+WHERE published_year < 2000;
